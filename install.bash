914c8b22a909d18ae8c0a6f3f4958e028107528f
Ed Drouillard <edrulrd@hotmail.com>
2025-08-14 19:31:57 -0400

    testfile update

--- install.bash
+++ install.bash
@@ -2,10 +2,10 @@
 
 # This script should be runable by running the following command in a bash terminal window:
 #
-# bash -c "$(curl -fsSL https://raw.githubusercontent.com/edrulrd/bash-setup/main/testscript.bash)"
+# bash -c "$(curl -fsSL https://raw.githubusercontent.com/edrulrd/bash-setup/main/install.bash)"
 #
 # or
-# bash -c "$(wget https://raw.githubusercontent.com/edrulrd/bash-setup/main/testscript.bash -O -)"
+# bash -c "$(wget https://raw.githubusercontent.com/edrulrd/bash-setup/main/install.bash -O -)"
 
 echo Hi Ed
 echo This was run from github.com
