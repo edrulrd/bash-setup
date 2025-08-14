38f38302860c84c7fd9a3da869bd84eda5943e93
Ed Drouillard <edrulrd@hotmail.com>
2025-08-14 18:46:36 -0400

    initial commit

---
+++ install.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This script should be runable by running the following command in a bash terminal window:
+#
+# bash -c "$(curl -fsSL https://raw.githubusercontent.com/edrulrd/bash-setup/main/testscript.bash)"
+#
+# or
+# bash -c "$(wget https://raw.githubusercontent.com/edrulrd/bash-setup/main/testscript.bash -O -)"
+
+echo Hi Ed
+echo This was run from github.com
+
+neofetch
+
+exit
